9d574bdfc84c4d727f88076e057e57a269d0ade2
TrujiDev <eetgrisales@gmail.com>
2024-01-21 18:12:22 -0500

    Refactor TodoRoutes controller import and add getTodoById route

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { TodosController } from './controller';
+import { TodoController } from './controller';
 
 export class TodoRoutes {
 	static get routes(): Router {
 		const router = Router();
-		const todoController = new TodosController();
+		const todoController = new TodoController();
 
 		router.get('/', todoController.getTodos);
+		router.get('/:id', todoController.getTodoById);
 
 		return router;
 	}
