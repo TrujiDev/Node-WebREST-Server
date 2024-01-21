7de4b19ea8c76c3112ec11b3799d267ad9615780
TrujiDev <eetgrisales@gmail.com>
2024-01-21 17:21:55 -0500

    Add AppRouter routes to Server configuration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from './config/envs';
 import { Server } from './presentation/Server';
+import { AppRouter } from './presentation/routes';
 
 (async () => {
 	main();
@@ -8,6 +9,7 @@ import { Server } from './presentation/Server';
 function main() {
 	const server = new Server({
 		port: envs.PORT,
+		routes: AppRouter.routes,
 		public_path: envs.PUBLIC_PATH,
 	});
 	server.start();
