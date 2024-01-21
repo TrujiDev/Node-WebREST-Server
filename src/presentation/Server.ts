8d95b8d44b29c0e12364f1f9c4aabbab6a43f439
TrujiDev <eetgrisales@gmail.com>
2024-01-21 17:22:11 -0500

    Add routes to Server class constructor

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,20 +1,23 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
 	port: number;
+	routes: Router;
 	public_path?: string;
 }
 
 export class Server {
 	private app = express();
 	private readonly port: number;
+	private readonly routes: Router;
 	private readonly publicPath: string;
 
 	constructor(options: Options) {
-		const { port, public_path = 'public' } = options;
+		const { port, routes, public_path = 'public' } = options;
 
 		this.port = port;
+		this.routes = routes;
 		this.publicPath = public_path;
 	}
 
@@ -22,6 +25,10 @@ export class Server {
 		// Middleware
 		this.app.use(express.static(this.publicPath));
 
+		// Routes
+		this.app.use(this.routes);
+
+		// SPA
 		this.app.get('*', (req, res) => {
 			const indexPath = path.join(
 				__dirname,
