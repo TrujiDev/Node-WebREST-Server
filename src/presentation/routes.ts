f91dcb933288b0773bb8a3ce8acc3e3fab76f321
TrujiDev <eetgrisales@gmail.com>
2024-01-21 17:45:51 -0500

    Refactor routes to use TodoRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
+import { TodoRoutes } from './todos/routes';
 
 export class AppRouter {
 	static get routes(): Router {
 		const router = Router();
 
-		router.get('/api/todos', (req, res) => {
-			return res.json([
-				{ id: 1, title: 'Buy Milk', created_at: new Date() },
-				{ id: 2, title: 'Buy Bread', created_at: null },
-				{ id: 3, title: 'Buy Eggs', created_at: new Date() },
-			]);
-		});
+		router.use('/api/todos', TodoRoutes.routes);
 
 		return router;
 	}
