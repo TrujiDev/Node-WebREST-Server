282d3a7dd073588e32d6d41c5ef602b573087422
TrujiDev <eetgrisales@gmail.com>
2024-01-21 19:24:49 -0500

    Update todo controller to use "completed_at" instead of "created_at"

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 
 const todos = [
-	{ id: 1, title: 'Buy Milk', created_at: new Date() },
-	{ id: 2, title: 'Buy Bread', created_at: null },
-	{ id: 3, title: 'Buy Eggs', created_at: new Date() },
-	{ id: 4, title: 'Buy Butter', created_at: new Date() },
-	{ id: 5, title: 'Buy Cheese', created_at: null },
-	{ id: 6, title: 'Buy Water', created_at: new Date() },
+	{ id: 1, title: 'Buy Milk', completed_at: new Date() },
+	{ id: 2, title: 'Buy Bread', completed_at: null },
+	{ id: 3, title: 'Buy Eggs', completed_at: new Date() },
+	{ id: 4, title: 'Buy Butter', completed_at: new Date() },
+	{ id: 5, title: 'Buy Cheese', completed_at: null },
+	{ id: 6, title: 'Buy Water', completed_at: new Date() },
 ];
 
 export class TodoController {
@@ -32,8 +32,27 @@ export class TodoController {
 		if (!title)
 			return res.status(400).json({ message: 'Title argument is missing' });
 
-		const newTodo = { id: todos.length + 1, title, created_at: new Date() };
+		const newTodo = { id: todos.length + 1, title, completed_at: new Date() };
 		todos.push(newTodo);
 		return res.json(newTodo);
+	}
+
+	public updateTodoById(req: Request, res: Response) {
+		const id = +req.params.id;
+		if (isNaN(id))
+			return res.status(400).json({ message: 'ID argument is not a number' });
+
+		const todo = todos.find(todo => todo.id === id);
+		if (!todo)
+			return res.status(404).json({ message: `todo with ID ${id} is not found` });
+
+		const { title, completed_at } = req.body;
+
+		todo.title = title || todo.title;
+		completed_at === null
+			? (todo.completed_at = null)
+			: (todo.completed_at = new Date(completed_at || todo.completed_at));
+
+		return res.json(todo);
 	}
 }
