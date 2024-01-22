2841268d4c5139253ffaa105500a1654164b9d98
TrujiDev <eetgrisales@gmail.com>
2024-01-22 15:41:28 -0500

    Add TodoDatasource and TodoRepository interfaces

---
+++ src/domain/datasources/todo.datasource.ts
@@ -0,0 +1,12 @@
+import { CreateDtoTodo, UpdateDtoTodo } from '../dtos';
+import { TodoEntity } from '../entities/todo.entity';
+
+export abstract class TodoDatasource {
+	abstract create(createTodoDto: CreateDtoTodo): Promise<TodoEntity>;
+
+	abstract getAll(): Promise<TodoEntity[]>;
+
+	abstract findById(id: number): Promise<TodoEntity | undefined>;
+	abstract updateById(updateTodoDto: UpdateDtoTodo): Promise<TodoEntity>;
+	abstract deleteById(id: number): Promise<TodoEntity>;
+}
