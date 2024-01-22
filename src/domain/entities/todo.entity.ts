16f1e04a8644369e8a6a753f72c34f42bc784bb4
TrujiDev <eetgrisales@gmail.com>
2024-01-22 17:09:32 -0500

    Add static method fromObj to TodoEntity

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -8,4 +8,20 @@ export class TodoEntity {
 	get isCompleted() {
 		return !!this.completed_at;
 	}
+
+	public static fromObj(object: { [key: string]: any }): TodoEntity {
+		const { id, title, completed_at } = object;
+
+		if (!id) throw new Error('id is required');
+		if (!title) throw new Error('title is required');
+
+		let newCompletedAt;
+		if (completed_at) {
+			newCompletedAt = new Date(completed_at);
+			if (isNaN(newCompletedAt.getTime()))
+				throw new Error('completed_at must be a valid date');
+		}
+
+		return new TodoEntity(id, title, completed_at);
+	}
 }
