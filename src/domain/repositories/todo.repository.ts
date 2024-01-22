51abf8ea9249f5c2a2cdd89e66d5eef5f7f10066
TrujiDev <eetgrisales@gmail.com>
2024-01-22 17:09:37 -0500

    Update TodoRepository interface with correct DTO names

--- src/domain/repositories/todo.repository.ts
+++ src/domain/repositories/todo.repository.ts
@@ -1,12 +1,12 @@
-import { CreateDtoTodo, UpdateDtoTodo } from '../dtos';
+import { CreateTodoDto, UpdateTodoDto } from '../dtos';
 import { TodoEntity } from '../entities/todo.entity';
 
 export abstract class TodoRepository {
-	abstract create(createTodoDto: CreateDtoTodo): Promise<TodoEntity>;
+	abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
 
 	abstract getAll(): Promise<TodoEntity[]>;
 
 	abstract findById(id: number): Promise<TodoEntity | undefined>;
-	abstract updateById(updateTodoDto: UpdateDtoTodo): Promise<TodoEntity>;
+	abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
 	abstract deleteById(id: number): Promise<TodoEntity>;
 }
