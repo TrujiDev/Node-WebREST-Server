02e11beb8f5aaf5de208ce64ce45e11897742872
TrujiDev <eetgrisales@gmail.com>
2024-01-23 11:28:07 -0500

    Refactor findById method signature in TodoRepository

--- src/domain/repositories/todo.repository.ts
+++ src/domain/repositories/todo.repository.ts
@@ -3,9 +3,8 @@ import { TodoEntity } from '../entities/todo.entity';
 
 export abstract class TodoRepository {
 	abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
-
 	abstract getAll(): Promise<TodoEntity[]>;
-	abstract findById(id: number): Promise<TodoEntity | undefined>;
+	abstract findById(id: number): Promise<TodoEntity>;
 	abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
 	abstract deleteById(id: number): Promise<TodoEntity>;
 }
